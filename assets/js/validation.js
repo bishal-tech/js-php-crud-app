43054f5897a71f9b8fa23091eb59a53e22afc62a
Bishal <bishal.tech.com@gmail.com>
2023-05-20 20:37:44 +0530

    add php submit form

--- assets/js/validation.js
+++ assets/js/validation.js
@@ -19,5 +19,42 @@ $(document).ready(function () {
         required: "Please enter your email",
       },
     },
+
+    submitHandler: function (form) {
+      // Perform actions after validation and before form submission
+
+      // Update the table using JavaScript/jQuery
+    //   var name = $("#name").val();
+    //   var email = $("#email").val();
+
+      const title = document.querySelector("#title").value;
+      const desp = document.querySelector("#description").value;
+      const file = document.querySelector("#files").value;
+
+      // Example: Create a new row and append it to the table
+      var newRow = `<tr>
+                <td>${srNum}</td>
+                <td>${title}</td>
+                <td>${desp}</td>
+                <td>${file}</td>
+                <td>
+                    <button class="btn btn-sm btn-success" onclick="editRow(this)">Edite</button>
+                    <button class="btn btn-sm btn-primary" onclick="deleteRow(this)">Delete</button>
+                </td>
+            </tr>`;
+
+      $("#tableBody").append(newRow);
+
+      // Send form data to the PHP script using AJAX
+      $.ajax({
+        url: $(form).attr("action"),
+        type: $(form).attr("method"),
+        data: $(form).serialize(),
+        success: function (response) {
+          // Handle the response from the PHP script
+          console.log(response);
+        },
+      });
+    },
   });
 });
